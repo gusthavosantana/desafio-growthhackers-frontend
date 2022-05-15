53fc3214dfb76ef124ba331cec6ddb9e308d5443
Gusthavo Santana <gusthavosantana@gmail.com>
2022-05-15 00:32:00 -0300

    add routes and state provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
-import { makeServer } from "./services/miragejs"
+import Categories from './pages/categories/List';
+import Products from './pages/products/List';
+import CategoryRegister from './pages/categories/Register';
+import ProductRegister from './pages/products/Register';
+import { Toaster } from 'react-hot-toast';
+import { RootProvider } from './providers';
+// import Products from './pages/products/List';
+// import { makeServer } from './services/miragejs';
 
-if (process.env.NODE_ENV === 'development') {
-  makeServer({ environment: process.env.NODE_ENV });
-}
+// if (process.env.NODE_ENV === 'development') {
+//   makeServer({ environment: process.env.NODE_ENV });
+// }
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App>
+        <RootProvider>
+            <Routes>
+              <Route path="/" element={<Categories />} />
+              <Route path="/categories" element={<Categories />} />
+              <Route path="/categories/:category/products/register" element={<ProductRegister />} />
+              <Route path="/categories/:category/products/:id/edit" element={<ProductRegister />} />
+              <Route path="/categories/register" element={<CategoryRegister />} />
+              <Route path="/categories/:id/edit" element={<CategoryRegister />} />
+              <Route path="/categories/:category/products" element={<Products />} />
+            </Routes>
+        </RootProvider>
+        <Toaster />
+      </App>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
